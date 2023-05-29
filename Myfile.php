b2449a71c4955f2833587ba5917d7ffa52c0a44b
UnikOgugua <133266801+UnikOgugua@users.noreply.github.com>
2023-05-29 17:22:22 +0100

    Create Myfile.php

---
+++ Myfile.php
@@ -0,0 +1,13 @@
+<?php
+// Student information
+$name = "Ori Ogugua Nwoshibe";
+$regNumber = "2021/HND/33844/CS";
+$stateOfOrigin = "Ebonyi state";
+$projectTopic = "Design and implementation of an expert system to handle student complaints";
+
+// Output the information
+echo "Name: " . $name . "\n";
+echo "Registration Number: " . $regNumber . "\n";
+echo "State of Origin: " . $stateOfOrigin . "\n";
+echo "Project Topic: " . $projectTopic . "\n";
+?>
